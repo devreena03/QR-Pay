06d71a31eb030309815165f5bdb7a3f73b33aaeb
Reena <rkreena07@gmail.com>
2019-06-10 11:58:18 +0530

    updated file path

--- services/qrcodeGenerator.js
+++ services/qrcodeGenerator.js
@@ -7,7 +7,7 @@ var qrGenerator = function(qr_txt) {
     var qr_png = qr.imageSync(qr_txt,{ type: 'png'})
     let qr_code_file_name = qr_txt.match(/token=EC-(.*)/)[1] + '.png';
  
-    fs.writeFileSync(path.join(__dirname, '..','public','qr',  qr_code_file_name), qr_png, (err) => {      
+    fs.writeFileSync(path.join(__dirname, '../public/qr/'+qr_code_file_name), qr_png, (err) => {      
         if(err){
             console.log(err);
         }      
